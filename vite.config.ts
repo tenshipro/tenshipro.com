0526e607c56418169a28320c36ea8164aedb9128
killbasa <git@killbasa.com>
2025-09-30 22:33:26 -0400

    cloudflare workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import type { CommonServerOptions } from 'vite';
+
+const serverOptions: CommonServerOptions = {
+	port: 5300
+};
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: {
-		port: 4000
-	},
-	preview: {
-		port: 4000
-	}
+	server: serverOptions,
+	preview: serverOptions
 });
